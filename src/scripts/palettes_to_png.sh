fd42365919a141643637b7738e5f30ab42f07e75
Barry <91902180+barrymode@users.noreply.github.com>
2021-05-29 01:00:12 -0500

    [+] `palettes_to_png.sh` script

---
+++ src/scripts/palettes_to_png.sh
@@ -0,0 +1,6 @@
+for i in {1..251}; do
+  /Applications/Firefox.app/Contents/MacOS/firefox -screenshot \
+  ~/Documents/Computers/Repos/pokepalettes/build/png/$i.png \
+  file:///Users/Barry/Documents/Computers/Repos/pokepalettes/build/html/$i.html \
+  --window-size=1000,1000
+done
